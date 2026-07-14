cae0bef25d0ed99f7ffd36a58821b8bbdc63e92e
basilsa77 <basilsa77@users.noreply.github.com>
2026-07-14 06:12:32 +0300

    fix(v15): satisfy strict cloud sync analysis

--- lib/services/cloud_sync.dart
+++ lib/services/cloud_sync.dart
@@ -7,7 +7,6 @@ import 'dart:async';
 import 'dart:convert';
 
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 
 import 'auth_service.dart';
